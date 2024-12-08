699fc61c27d8a0f082bec44a3f703c44656b205c
TJ Draper <tj@buzzingpixel.com>
2024-12-07 22:09:08 -0600

    Set up PHP code standards

--- docker/api/pre-up-provisioning.sh
+++ docker/api/pre-up-provisioning.sh
@@ -5,9 +5,9 @@ DOCKER_DIR=$(dirname "${SCRIPT_DIR}");
 PROJ_DIR=$(dirname "${DOCKER_DIR}");
 API_DIR="${PROJ_DIR}/api";
 
-#docker run -it --rm \
-#    --entrypoint "" \
-#    --name api-provision \
-#    -v "${API_DIR}:/var/www" \
-#    -w /var/www \
-#    ghcr.io/st-mark-reformed/smrc-2025-api sh -c "composer install";
+docker run -it --rm \
+    --entrypoint "" \
+    --name api-provision \
+    -v "${API_DIR}:/var/www" \
+    -w /var/www \
+    ghcr.io/st-mark-reformed/smrc-2025-api sh -c "composer install";
