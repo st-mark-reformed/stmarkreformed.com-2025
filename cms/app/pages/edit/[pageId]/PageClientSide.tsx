2f38e2d94c4880f2de6b4e2565d795719cfda6d3
TJ Draper <tj@buzzingpixel.com>
2024-12-30 12:43:24 -0600

    Do not display page controls for Menu Parent Only and Menu Links

--- cms/app/pages/edit/[pageId]/PageClientSide.tsx
+++ cms/app/pages/edit/[pageId]/PageClientSide.tsx
@@ -3,7 +3,11 @@
 import React, { useMemo, useState } from 'react';
 import { CheckIcon } from '@heroicons/react/20/solid';
 import { v4 as uuid } from 'uuid';
-import { PageStatus, PageTypeWithDataNoChildrenFrontEnd } from '../../PageType';
+import {
+    PageType as PageTypeType,
+    PageStatus,
+    PageTypeWithDataNoChildrenFrontEnd,
+} from '../../PageType';
 import useDataManager from './useDataManager';
 import { FrontEndPageContext } from '../../../FrontEndPageContext';
 import PageHeader from '../../../layout/PageHeader';
@@ -81,6 +85,12 @@ export default function PageClientSide (
         };
     }
 
+    let showPageControls = true;
+
+    if (data.type === PageTypeType.menu_link || data.type === PageTypeType.menu_parent_only) {
+        showPageControls = false;
+    }
+
     return (
         <FrontEndPageContext.Provider value={contextValue}>
             <form>
@@ -141,60 +151,80 @@ export default function PageClientSide (
                                 );
                             }}
                         />
-                        <Toggle
-                            label="Show In Menu?"
-                            name="showInMenu"
-                            value={data.showInMenu}
-                            setValue={setData}
-                        />
-                        <Toggle
-                            label="Show Sub-Page Sidebar?"
-                            name="showSubPageSidebar"
-                            value={data.showSubPageSidebar}
-                            setValue={setData}
-                        />
-                        <Toggle
-                            label="Use Short Hero?"
-                            name="useShortHero"
-                            value={data.useShortHero}
-                            setValue={setData}
-                        />
+                        {(() => {
+                            if (!showPageControls) {
+                                return null;
+                            }
+
+                            return (
+                                <>
+                                    <Toggle
+                                        label="Show In Menu?"
+                                        name="showInMenu"
+                                        value={data.showInMenu}
+                                        setValue={setData}
+                                    />
+                                    <Toggle
+                                        label="Show Sub-Page Sidebar?"
+                                        name="showSubPageSidebar"
+                                        value={data.showSubPageSidebar}
+                                        setValue={setData}
+                                    />
+                                    <Toggle
+                                        label="Use Short Hero?"
+                                        name="useShortHero"
+                                        value={data.useShortHero}
+                                        setValue={setData}
+                                    />
+                                </>
+                            );
+                        })()}
                     </div>
-                    <CustomHero data={data} setData={setData} />
-                    <div>
-                        <div className="block text-sm font-semibold leading-6 text-gray-900 pb-2">
-                            Hero Upper CTA
-                        </div>
-                        <div className="p-4 border">
-                            <Url
-                                value={data.heroUpperCta}
-                                setValue={(val) => {
-                                    // @ts-expect-error TS2345
-                                    setData('heroUpperCta', val);
-                                }}
-                            />
-                        </div>
-                    </div>
-                    <div className="align-top grid gap-4 sm:grid-cols-2">
-                        <TextInput
-                            label="Hero Heading"
-                            name="heroHeading"
-                            value={data.heroHeading}
-                            setValue={setData}
-                        />
-                        <TextInput
-                            label="Hero Sub-heading"
-                            name="heroSubheading"
-                            value={data.heroSubheading}
-                            setValue={setData}
-                        />
-                    </div>
-                    <TextArea
-                        label="Hero Paragraph"
-                        name="heroParagraph"
-                        value={data.heroParagraph}
-                        setValue={setData}
-                    />
+                    {(() => {
+                        if (!showPageControls) {
+                            return null;
+                        }
+
+                        return (
+                            <>
+                                <CustomHero data={data} setData={setData} />
+                                <div>
+                                    <div className="block text-sm font-semibold leading-6 text-gray-900 pb-2">
+                                        Hero Upper CTA
+                                    </div>
+                                    <div className="p-4 border">
+                                        <Url
+                                            value={data.heroUpperCta}
+                                            setValue={(val) => {
+                                                // @ts-expect-error TS2345
+                                                setData('heroUpperCta', val);
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="align-top grid gap-4 sm:grid-cols-2">
+                                    <TextInput
+                                        label="Hero Heading"
+                                        name="heroHeading"
+                                        value={data.heroHeading}
+                                        setValue={setData}
+                                    />
+                                    <TextInput
+                                        label="Hero Sub-heading"
+                                        name="heroSubheading"
+                                        value={data.heroSubheading}
+                                        setValue={setData}
+                                    />
+                                </div>
+                                <TextArea
+                                    label="Hero Paragraph"
+                                    name="heroParagraph"
+                                    value={data.heroParagraph}
+                                    setValue={setData}
+                                />
+                            </>
+                        );
+                    })()}
                     <PageTypeFactory
                         type={data.type}
                         data={data.data}
